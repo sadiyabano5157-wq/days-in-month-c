337a5bbe54e42a7248cf6022db0a182e77d47daf
sadiyabano5157-wq <sadiyabano5157@gmail.com>
2026-05-07 19:31:33 +0530

    Add program to determine days in a month

---
+++ days_in_month.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int month;
+
+    printf("Enter month number:\n");
+    scanf("%d",&month);
+
+    if(month==1 || month==3 || month==5 ||
+       month==7 || month==8 || month==10 || month==12)
+    {
+        printf("31 days\n");
+    }
+    else if(month==2)
+    {
+        printf("28 or 29 days\n");
+    }
+    else if(month==4 || month==6 || month==9 || month==11)
+    {
+        printf("30 days\n");
+    }
+    else
+    {
+        printf("Invalid month number\n");
+    }
+
+    return 0;
+}
